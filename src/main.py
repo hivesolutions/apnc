e3514d18c897e89d775892e3a608d20c003844a3
João Magalhães <joamag@gmail.com>
2012-10-31 20:31:45 +0000

    small code fix

--- src/main.py
+++ src/main.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Hive Colony Framework
+# Hive Apple Push Client
 # Copyright (c) 2008-2012 Hive Solutions Lda.
 #
-# This file is part of Hive Colony Framework.
+# This file is part of Hive Apple Push Client.
 #
-# Hive Colony Framework is free software: you can redistribute it and/or modify
+# Hive Apple Push Client is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hive Colony Framework is distributed in the hope that it will be useful,
+# Hive Apple Push Client is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+# along with Hive Apple Push Client. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "João Magalhães <joamag@hive.pt> & Tiago Silva <tsilva@hive.pt>"
 """ The author(s) of the module """
